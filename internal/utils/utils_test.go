04c3eab35606dd900d4c351a85447782112b5317
Valery <closable@gmail.com>
2024-02-05 14:45:53 +0300

    fix statictest names

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -10,29 +10,29 @@ func TestValidateURL(t *testing.T) {
 
 	tests := []struct {
 		name   string
-		txtUrl string
+		txtURL string
 		want   bool
 	}{
 		// TODO: Add test cases.
 		{
 			name:   "Check URL is valid",
-			txtUrl: "http://yandex.ru",
+			txtURL: "http://yandex.ru",
 			want:   true,
 		},
 		{
 			name:   "Check protocol",
-			txtUrl: "yandex.ru",
+			txtURL: "yandex.ru",
 			want:   false,
 		},
 		{
 			name:   "Check URL",
-			txtUrl: "dgdgdgdgdfgdfg",
+			txtURL: "dgdgdgdgdfgdfg",
 			want:   false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, ValidateURL(tt.txtUrl), tt.want)
+			assert.Equal(t, ValidateURL(tt.txtURL), tt.want)
 
 		})
 	}
