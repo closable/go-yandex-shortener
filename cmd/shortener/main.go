bff1ed21216f79f7583c09a52a4f5ffe5c106b57
Valery <closable@gmail.com>
2024-02-06 13:40:45 +0300

    change standart to chi router + add client

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/closable/go-yandex-shortener/internal/handlers"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
@@ -14,58 +15,10 @@ func main() {
 }
 
 func run() error {
-	return http.ListenAndServe(`:8080`, http.HandlerFunc(handlers.Webhook))
+	r := chi.NewRouter()
+	r.Get("/{id}", handlers.GetEndpointByShortener)
+	r.Post("/", handlers.GenerateShortener)
+	return http.ListenAndServe(`:8080`, r)
+
+	// return http.ListenAndServe(`:8080`, http.HandlerFunc(handlers.Webhook))
 }
-
-/*func webhook(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "plain/text")
-	switch method := r.Method; method {
-	case http.MethodPost:
-		shortener := ""
-
-		info, err := io.ReadAll(r.Body)
-		if err != nil || len(info) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Error! Request body is empty!"))
-			return
-		}
-
-		if !(utils.ValidateURL(string(info))) {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Error! Check url it's should seems as like this 'http[s]://example.com'"))
-			return
-		}
-
-		shortener = storage.GetShortener(string(info))
-
-		w.WriteHeader(http.StatusCreated)
-		body := fmt.Sprintf("http://%s/%s", r.Host, shortener)
-		w.Write([]byte(body))
-
-	case http.MethodGet:
-		shortener := ""
-		path := r.URL.Path
-		if len(path) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Error! id is empty!"))
-			return
-
-		}
-		shortener = path[1:]
-
-		if !storage.FindExistingKey(shortener) || len(shortener) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Error! id is not found or empty"))
-			return
-		}
-
-		w.Header().Set("Location", storage.Storage.Urls[shortener])
-		w.WriteHeader(http.StatusTemporaryRedirect)
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Error! Please, check documentation again!"))
-		return
-	}
-}*/
