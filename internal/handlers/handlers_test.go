ec2149676bc281dcf654822344415fe0c8d289d8
Valery <closable@gmail.com>
2024-06-09 12:11:21 +0300

    add examples

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -414,3 +414,14 @@ func TestUploadBatch(t *testing.T) {
 		}
 	}
 }
+
+func ExampleGetUserID() {
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTI3MzgzOTAsIlVzZXJJRCI6NH0.V9WdWdJWeU1qqVCGDfTGu0asPZhiFUPmtnsfpN0GPro"
+	//userID := 5
+	out1 := GetUserID(tokenString)
+	fmt.Println(out1)
+
+	//Output
+	//5
+
+}
