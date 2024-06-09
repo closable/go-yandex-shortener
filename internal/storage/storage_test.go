221f59abeff8953362b90e3bd623972e2b28c444
Valery <closable@gmail.com>
2024-06-09 10:14:40 +0300

    add benchmarks

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,4 +35,27 @@ func TestFindKeyByValue(t *testing.T) {
 
 		})
 	}
+}
+
+func BenchmarkFindKeyByValue(b *testing.B) {
+	store, _ := NewMemory()
+	store.Urls["abcd0"] = "http://0mail.ru"
+	store.Urls["abcd1"] = "http://1mail.ru"
+	store.Urls["abcd2"] = "http://2mail.ru"
+	store.Urls["abcd3"] = "http://3mail.ru"
+	store.Urls["abcd4"] = "http://4mail.ru"
+	store.Urls["abcd5"] = "http://5mail.ru"
+	store.Urls["abcd6"] = "http://6mail.ru"
+	store.Urls["abcd7"] = "http://7mail.ru"
+	store.Urls["abcd8"] = "http://8mail.ru"
+	store.Urls["abcd9"] = "http://98mail.ru"
+
+	for i := 0; i < b.N; i++ {
+		store.FindKeyByValue(getKeyforBench())
+	}
+}
+
+func getKeyforBench() string {
+	r := rand.Intn(9)
+	return fmt.Sprintf("abcd%d", r)
 }
